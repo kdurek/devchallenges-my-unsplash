49641bb35aaeaa952b1d658c98218ea20ec7ee87
Krzysztof Durek <walendes@gmail.com>
2021-01-07 13:36:21 +0100

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  purge: ["./src/pages/**/*.js", "./src/components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
@@ -44,7 +44,7 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: { visibility: ["group-hover"] },
+    extend: {},
   },
   plugins: [],
 }
